fcfa9922f9def5d088ff6619d33ebf6b411414cb
Ong Li Min <134113673+flyingcat9@users.noreply.github.com>
2025-08-20 14:37:44 +0800

    Added a list to store contents.

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,6 +1,23 @@
+import java.util.ArrayList;
+
 public class Echo {
+
+    protected ArrayList<String> taskList = new ArrayList<>();
+
+    public void addToList(String s) {
+        taskList.add(s);
+        System.out.print("added: ");
+        echo(s);
+    }
+
 
     public static void echo(String word) {
         System.out.println(word);
+    }
+
+    public void printList() {
+        for (int i = 0; i < taskList.size(); i++ ) {
+            System.out.println(String.valueOf(i+1) + ". " +  taskList.get(i));
+        }
     }
 }
--- src/main/java/UserInput.java
+++ src/main/java/UserInput.java
@@ -7,12 +7,22 @@ public class UserInput {
         System.out.println("Hello! I'm Jocelyn. What can I do for you?");
         Scanner object = new Scanner(System.in);
         String userWords = object.nextLine();
+        Echo text = new Echo();
         while (!userWords.equals("bye")) {
-            Echo.echo(userWords);
+            validityOfWords(userWords, text);
             object = new Scanner(System.in);
             userWords = object.nextLine();
         }
         System.out.println("Bye, I hope to see you soon!");
 
     }
+
+    public static void validityOfWords(String s, Echo e) {
+        if (s.equals("list")) {
+            e.printList();
+        } else {
+            e.addToList(s);
+        }
+    }
 }
+
